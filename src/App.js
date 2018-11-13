7670a2d1ebe96097560aad158c4d2f31f987e09c
Hope R <nadezhda.lkn@yahoo.com>
2018-11-13 16:25:04 +0300

    Make it ethic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-            <h1>Hola, byatch</h1>
+            <h1>Hola, chikito</h1>
             </div>
         )
     }
